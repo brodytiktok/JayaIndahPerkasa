be211616e6210c4aa8dc0cb34ced28fbf794e3e4
brodytiktok <kevinpratama161000@gmail.com>
2022-06-10 09:53:41 +0700

    a

--- scfiles/resources/views/dashboard/index.blade.php
+++ scfiles/resources/views/dashboard/index.blade.php
@@ -8,8 +8,8 @@
         <div class="card card-widget widget-user">
             <!-- Add the bg color to the header using any of the bg-* classes -->
             <div class="widget-user-header text-white" style="background: url('../dist/img/photo1.png') center center;">
-                <h3 class="widget-user-username text-right">{{ Auth::user()->name }}</h3>
-                <h5 class="widget-user-desc text-right">Web Designer</h5>
+                <h4 class="widget-user-username text-right">{{ Auth::user()->name }}</h4>
+                
             </div>
             <div class="widget-user-image">
                 <img class="img-circle" src="../../dist/img/people.jpg" alt="User Avatar">
